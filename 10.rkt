cfca360c861c5dcebc84ed8a6c916d61c24c7624
Guillaume Coré (fridim) <guillaume.core@gmail.com>
2013-07-31 23:49:33 +0000

    10: remove trailing space

--- 10.rkt
+++ 10.rkt
@@ -1,7 +1,7 @@
 ;; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 ;; Find the sum of all the primes below two million.
 
-#lang racket 
+#lang racket
 
 (define (get-primes n)
   (let get-primes2 ([l (range 2 n)])
@@ -11,6 +11,6 @@
              [lsp (filter (lambda (n) (not (= (modulo n i) 0))) l)])
         (if (> (* i i) n)
           lsp
-          (cons i (get-primes2 lsp))))))) 
+          (cons i (get-primes2 lsp)))))))
 
 (time (apply + (get-primes 2000000)))
