6c0645478949a1093cb0e7a8405a908c9267a9ad
Guillaume Coré (fridim) <g@fridim.org>
2013-08-19 01:27:26 +0000

    problem 20

---
+++ 20.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+#|
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+|#
+
+(define (fac n)
+  (if (= n 1) 1
+    (* n (fac (- n 1)))))
+
+(apply + (filter number?
+                 (map string->number
+                      (string-split
+                        (number->string (fac 100))
+                        ""))))
