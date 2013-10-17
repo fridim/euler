8a9eb2d38d78cc44f0b408ce8d8e56761b2f6288
Guillaume Coré <guillaume.core@gmail.com>
2013-10-16 20:55:50 -0400

    problem 28

---
+++ 28.rkt
@@ -0,0 +1,25 @@
+#lang racket/base
+
+#|
+Starting with the number 1 and moving to the right in a clockwise direction a 5
+by 5 spiral is formed as follows:
+
+21 22 23 24 25
+20  7  8  9 10
+19  6  1  2 11
+18  5  4  3 12
+17 16 15 14 13
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed
+in the same way?
+|#
+
+
+(define (euler28 limit)
+  (+ 1 (let helper ([n 0] [sum 0] [prev 1])
+         (let ((diagn (+ prev (* 2 (+ 1 (quotient n 4))))))
+           (if (> diagn (* limit limit))
+             sum
+             (helper (+ n 1) (+ sum diagn) diagn))))))
