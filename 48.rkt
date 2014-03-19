ada2b16decef4d06c0469b52e743192eb905b54b
Guillaume Coré (fridim) <g@fridim.org>
2014-03-18 23:20:00 -0400

    problem 48

---
+++ 48.rkt
@@ -0,0 +1,14 @@
+#lang racket/base
+
+#| The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000. |#
+
+(define (euler48)
+  (modulo
+    (for/sum ((i (in-range 1 1001)))
+           (expt i i))
+    10000000000))
+
+
+(time (euler48)) ; 10ms
